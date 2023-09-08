3afdced4f74bf71c7ba2f7e2d6795406d06c2b0e
TalelMejri <talelmejri8@gmail.com>
2023-09-08 14:19:58 +0100

    RadioButton

---
+++ lib/chocieList.dart
@@ -0,0 +1,5 @@
+enum choicList{
+  plane,
+  boat,
+  car
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'chocieList.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  bool switchdata=false;
+   bool switchdata=false;
+   choicList _radiolist=choicList.plane;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,12 +48,38 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
           const Text("User Choice"),
           const Divider(color: Colors.blue),
+          const Text("Switch"),
           Switch(value: switchdata, onChanged: (val){
             setState(() {
               switchdata=val;
             });
           }),
-          Text(switchdata ? 'On' : 'Off')
+          Text(switchdata ? 'On' : 'Off'),
+          const Divider(color: Colors.blue),
+          const Text("Radio"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Radio(value: choicList.car, groupValue: _radiolist, onChanged: (val){
+                 setState(() {
+                      _radiolist=choicList.car;
+                 });
+              }),
+                Text("Car",style: _radiolist==choicList.car ? const TextStyle(fontWeight: FontWeight.bold) : const TextStyle(),),
+                Radio(value: choicList.plane, groupValue: _radiolist, onChanged: (val){
+                 setState(() {
+                      _radiolist=choicList.plane;
+                 });
+              }),
+            Text("Plane",style: _radiolist==choicList.plane ? const TextStyle(fontWeight: FontWeight.bold) : const TextStyle(),),
+                Radio(value: choicList.boat, groupValue: _radiolist, onChanged: (val){
+                 setState(() {
+                      _radiolist=choicList.boat;
+                 });
+              }),
+              Text("Boat",style: _radiolist==choicList.boat ? const TextStyle(fontWeight: FontWeight.bold) : const TextStyle(),),
+            ],
+          )
           
         ]),
       ),
